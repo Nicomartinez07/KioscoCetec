f02eafed5c9c4a1877d5aeaa63c2ec618173aaee
Nicolas Martinez <nicolasmartinezalfonso@gmail.com>
2024-09-10 16:54:59 -0300

    Empezamos la estructura del abm

---
+++ scriptABM.sql
@@ -0,0 +1,40 @@
+INSERT INTO Reserva (Id, Id_usuario, Id_producto, Monto)
+VALUES ();
+
+UPDATE Reserva
+SET Monto
+WHERE 
+
+DELETE FROM Reserva
+WHERE condiciones;
+
+/* ------------------------------------------------------------------------------ */
+INSERT INTO Reserva (Id, Id_usuario, Id_producto, Monto)
+VALUES ();
+
+INSERT INTO Reserva (Id, Id_usuario, Id_producto, Monto)
+VALUES ();
+
+INSERT INTO Reserva (Id, Id_usuario, Id_producto, Monto)
+VALUES ();
+
+INSERT INTO Reserva (Id, Id_usuario, Id_producto, Monto)
+VALUES ();
+
+INSERT INTO Reserva (Id, Id_usuario, Id_producto, Monto)
+VALUES ();
+
+INSERT INTO Reserva (Id, Id_usuario, Id_producto, Monto)
+VALUES ();
+
+INSERT INTO Reserva (Id, Id_usuario, Id_producto, Monto)
+VALUES ();
+
+INSERT INTO Reserva (Id, Id_usuario, Id_producto, Monto)
+VALUES ();
+
+INSERT INTO Reserva (Id, Id_usuario, Id_producto, Monto)
+VALUES ();
+
+INSERT INTO Reserva (Id, Id_usuario, Id_producto, Monto)
+VALUES ();
